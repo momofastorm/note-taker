8e6584fd7c87bc2d8fbdb07c8927cfbf806573c5
momofastorm <marcyrappaport.blessed@gmail.com>
2023-06-27 15:12:28 -0500

    all working but delete

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,28 @@
-const router = require('express').Router();
+const { writeFile } = require('fs');
 const data = require('../../db/db.json');
+const router = require('express').Router();
 
 router.get('/notes', (req,res) => {
     res.json(data);
 });
 
+router.post('/notes', ({body},res) => {
+    body.id = Math.floor((Math.random() * 10000)).toString(16)
+    data.push(body);
 
+    writeFile('./db/db.json', JSON.stringify(data), err => {
+        if(err) console.log(err);
+        res.json(data);
+    });
+});
 
+router.delete('/notes/:id', ({params}, res) => {
+    data.filter(({id}) => {id != params.id})
+    
+    writeFile('./db/db.json', JSON.stringify(data), err => {
+        if(err) console.log(err);
+        res.json(data);
+    });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 const PORT = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 app.use(require('./routes'));
 
